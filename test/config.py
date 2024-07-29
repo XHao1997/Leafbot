227fba1d0c996c0054ed99f67825eb7c8da938ff
Xu, Hao <hao.xu@wur.nl>
2024-07-29 14:53:11 +0200

    upload cam part

--- test/config.py
+++
@@ -1,4 +0,0 @@
-import os
-
-# Get the absolute path of the project directory
-PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
--- test/test_file.py
+++
@@ -1,4 +0,0 @@
-import numpy as np
-for i in range(10):
-    with open('../data/imitation_car/cmd.txt', 'ab') as f:
-        np.savetxt(f,np.array([1]), delimiter=',')
